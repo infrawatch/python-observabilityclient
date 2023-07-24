c41a7d5fb3b48bf72ec72fb451f2bb8bc2d362c1
Jaromír Wysoglad <jwysogla@redhat.com>
2023-07-24 05:03:31 -0400

    Remove old observability client

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # python-observabilityclient
 
 observabilityclient is an OpenStackClient (OSC) plugin implementation that
-implements commands for management of OpenStack observability components such
-as Prometheus, collectd and Ceilometer.
+implements commands for management of Prometheus.
 
 ## Development
 
@@ -17,58 +16,4 @@ su - stack
 git clone https://github.com/infrawatch/python-observabilityclient
 cd python-observabilityclient
 sudo python setup.py install --prefix=/usr
-
-# clone and install observability playbooks and roles
-git clone https://github.com/infrawatch/osp-observability-ansible
-sudo mkdir /usr/share/osp-observability
-sudo ln -s `pwd`/osp-observability-ansible/playbooks /usr/share/osp-observability/playbooks
-sudo ln -s `pwd`/osp-observability-ansible/roles/spawn_container /usr/share/ansible/roles/spawn_container
-sudo ln -s `pwd`/osp-observability-ansible/roles/osp_observability /usr/share/ansible/roles/osp_observability
-```
-
-### Enable collectd write_prometheus
-Create a THT environment file to enable the write_prometheus plugin for the collectd service. Then redeploy your overcloud and include this new file:
-
-```
-mkdir -p ~/templates/observability
-cat <EOF >> templates/observability/collectd-write-prometheus.yaml
-resource_registry:
-  OS::TripleO::Services::Collectd: /usr/share/openstack-tripleo-heat-templates/deployment/metrics/collectd-container-puppet.yaml
-
-# TEST
-# parameter_merge_strategies:
-#   CollectdExtraPlugins: merge
-
-parameter_defaults:
-  CollectdExtraPlugins:
-    - write_prometheus
-EOF
-```
-
-### Discover endpoints
-After deployment of your cloud you can discover endpoints available for scraping:
-
-```
-source stackrc
-openstack observability discover --stack-name=standalone
-```
-
-### Deploy prometheus:
-Create a config file and run the setup command
-
-```
-$ cat test_params.yaml
-prometheus_remote_write:
-  stf:
-    url: https://default-prometheus-proxy-service-telemetry.apps.FAKE.ocp.cluster/api/v1/write
-    basic_user: internal
-    basic_pass: Pl4iNt3xTp4a55
-    ca_cert: |
-      -----BEGIN CERTIFICATE-----
-      ABCDEFGHIJKLMNOPQRSTUVWXYZ
-      -----END CERTIFICATE-----
-  not-stf:
-    url: http://prometheus-rw.example.com/api/v1/write
-
-$ openstack observability setup prometheus_agent --config ./test_params.yaml
 ```
--- observabilityclient/v1/base.py
+++ observabilityclient/v1/base.py
@@ -19,7 +19,6 @@ import shutil
 from osc_lib.command import command
 from osc_lib.i18n import _
 
-from observabilityclient.utils import runner
 from observabilityclient.utils import shell
 
 
@@ -44,68 +43,4 @@ class ObservabilityBaseCommand(command.Command):
             action='store_true',
             help=_("Disable cleanup of temporary files.")
         )
-        parser.add_argument(
-            '--workdir',
-            default=OBSWRKDIR,
-            help=_("Working directory for observability commands.")
-        )
-        parser.add_argument(
-            '--moduledir',
-            default=None,
-            help=_("Directory with additional Ansible modules.")
-        )
-        parser.add_argument(
-            '--ssh-user',
-            default='heat-admin',
-            help=_("Username to be used for SSH connection.")
-        )
-        parser.add_argument(
-            '--ssh-key',
-            default='/home/stack/.ssh/id_rsa',
-            help=_("SSH private key to be used for SSH connection.")
-        )
-        parser.add_argument(
-            '--ansible-cfg',
-            default=os.path.join(OBSWRKDIR, 'ansible.cfg'),
-            help=_("Path to Ansible configuration.")
-        )
-        parser.add_argument(
-            '--config',
-            default=None,
-            help=_("Path to playbook configuration file.")
-        )
         return parser
-
-    def _run_playbook(self, playbook, inventory, parsed_args):
-        """Run Ansible raw playbook"""
-        playbook = os.path.join(OBSLIBDIR, 'playbooks', playbook)
-        with shell.tempdir(parsed_args.workdir,
-                           prefix=os.path.splitext(playbook)[0],
-                           clear=not parsed_args.messy) as tmpdir:
-            # copy extravars file for the playbook run
-            if parsed_args.config:
-                envdir = shell.file_check(os.path.join(tmpdir, 'env'),
-                                          'directory')
-                shutil.copy(parsed_args.config,
-                            os.path.join(envdir, 'extravars'))
-            # copy inventory file for the playbook run
-            shutil.copy(inventory, os.path.join(tmpdir, 'inventory'))
-            # run playbook
-            rnr = runner.AnsibleRunner(tmpdir,
-                                       moduledir=parsed_args.moduledir,
-                                       ssh_user=parsed_args.ssh_user,
-                                       ssh_key=parsed_args.ssh_key,
-                                       ansible_cfg=parsed_args.ansible_cfg)
-            if parsed_args.messy:
-                print("Running playbook %s" % playbook)
-            rnr.run(playbook, debug=parsed_args.dev)
-            rnr.destroy(clear=not parsed_args.messy)
-
-    def _execute(self, command, parsed_args):
-        """Execute local command"""
-        with shell.tempdir(parsed_args.workdir, prefix='exec',
-                           clear=not parsed_args.messy) as tmpdir:
-            rc, out, err = shell.execute(command, workdir=tmpdir,
-                                         can_fail=parsed_args.dev,
-                                         use_shell=True)
-        return rc, out, err
